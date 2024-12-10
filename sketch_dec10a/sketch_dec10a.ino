e9e1091c0047a7559501a761fc50ff2b040bf659
Laumonier Gaëtan <gaetanlaumonier493@gmail.com>
2024-12-10 10:31:26 +0100

    Premier petit code Arduino

---
+++ sketch_dec10a/sketch_dec10a.ino
@@ -0,0 +1,22 @@
+const int DOOR_SENSOR_PIN = 5;
+int doorState;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(DOOR_SENSOR_PIN, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  doorState = digitalRead(DOOR_SENSOR_PIN);
+
+  if (doorState == HIGH) {
+    Serial.println("JOUR");
+  } else {
+    Serial.println("NUIT");
+  }
+  delay(100);
+}
